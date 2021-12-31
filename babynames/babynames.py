082b12683cb425d7315c54b3da73661c061cd4c6
lizat-i <ivan.lizat@outlook.de>
2021-12-31 03:47:04 +0100

    babynames function

--- babynames/babynames.py
+++ babynames/babynames.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
@@ -34,35 +34,60 @@ Suggested milestones for incremental development:
  -Fix main() to use the extract_names list
 """
 
+
+def get_file_as_list_of_words(filename):
+    f = open(filename, "rU")
+    text = f.read()
+    return text
+
+
+def find_helper(pat, tex, groupswitch=0):
+    match = re.search(r'%s' % pat, tex)
+    if match:
+        print(match.group(groupswitch))
+    else:
+        print('notFound!')
+
+
 def extract_names(filename):
-  """
+    """
   Given a file name for baby.html, returns a list starting with the year string
   followed by the name-rank strings in alphabetical order.
   ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
   """
-  # +++your code here+++
-  return
-
-
-def main():
-  # This command-line parsing code is provided.
-  # Make a list of command line arguments, omitting the [0] element
-  # which is the script itself.
-  args = sys.argv[1:]
-
-  if not args:
-    print 'usage: [--summaryfile] file [file ...]'
-    sys.exit(1)
-
-  # Notice the summary flag and remove it from args if it is present.
-  summary = False
-  if args[0] == '--summaryfile':
-    summary = True
-    del args[0]
-
-  # +++your code here+++
-  # For each filename, get the names, then either print the text output
-  # or write it to a summary file
+    teststring = get_file_as_list_of_words(filename='baby1990.html')
+    year  = re.search('Popularity in (\d\d\d\d)</h3>',teststring)
+    print year.groups(1)
+    match = re.findall('tr align="right"><td>(\d+)</td><td>(\w+)</td><td>(\w+)</td>', teststring)
+    dict_name_rank = {}
+    for rank_gend_tuple in match:
+        # TODO: add HERE AFTERWARDS TWO CHECK WRAPPERS IF THER entries are already given
+        # TODO: adnd if the ranke is higher ow lower then the list which is inside
+        dict_name_rank[rank_gend_tuple[1]] = rank_gend_tuple[0]
+        dict_name_rank[rank_gend_tuple[2]] = rank_gend_tuple[0]
+    year_names_and_ranks_sorted = sorted(dict_name_rank.items())
   
+    return year_names_and_ranks_sorted
+
+
+# . (dot) any char
+# \w word char
+# \d digit
+# \s whitespace
+# \S non-whitespace
+# + 1 or more
+# 0 or more
+def main():
+    args = sys.argv[1:]
+    if not args:
+        print 'usage: [--summaryfile] file [file ...]'
+        sys.exit(1)
+    summary = False
+    if args[0] == '--summaryfile':
+        summary = True
+        del args[0]
+    print args[0]
+    rny_list = extract_names(args[0])
+
 if __name__ == '__main__':
-  main()
+    main()
